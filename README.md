74c7c58bf246064d58cb7d14a27a3889fe321a8d
pcmhdhp <pmorr087@gmail.com>
2019-11-25 13:10:06 -0500

    Update and rename README.md to blogentry.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# Blog_entry_script
-Make a static website a bit more dynamic with a convenient bash scrip that'll make posts for you.
---
+++ blogentry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date=$1
+title=$2
+body=$3
+sed -i '21 a\
+	' index.html
+sed -i "22s|\(.*\)|<div class=\"main_top\"><h1>$date</h1></div><div class=\"main_body\"><p>$title</p><p>$body</p>\1|" index.html
+
+#sed -i '21 = returns after line 21, making line 22 blank.
+#sed '22s = edits the now blank line 22 with our new blog entry
+#put script in /var/www/html      syntax will be "sudo bash blogentry.sh "date-here" "title-here" "body-here"
