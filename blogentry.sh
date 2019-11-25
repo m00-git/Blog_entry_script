ab53a2b99434906ec68d75daa2c60186aa20f4c5
pcmhdhp <pmorr087@gmail.com>
2019-11-25 13:20:21 -0500

    Update blogentry.sh

--- blogentry.sh
+++ blogentry.sh
@@ -6,6 +6,7 @@ sed -i '21 a\
 	' index.html
 sed -i "22s|\(.*\)|<div class=\"main_top\"><h1>$date</h1></div><div class=\"main_body\"><p>$title</p><p>$body</p>\1|" index.html
 
+
 #sed -i '21 = returns after line 21, making line 22 blank.
 #sed '22s = edits the now blank line 22 with our new blog entry
 #put script in /var/www/html      syntax will be "sudo bash blogentry.sh "date-here" "title-here" "body-here"
